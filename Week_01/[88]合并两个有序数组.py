3aea211ec6c8cead49eba71a6d52e624cb610544
KathySkyer <42636545+KathySkyer@users.noreply.github.com>
2020-07-19 22:13:32 +0800

    Add files via upload

---
+++ Week_01/[88]合并两个有序数组.py
@@ -0,0 +1,46 @@
+# 给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。 
+# 
+#  
+# 
+#  说明: 
+# 
+#  
+#  初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。 
+#  你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）来保存 nums2 中的元素。 
+#  
+# 
+#  
+# 
+#  示例: 
+# 
+#  输入:
+# nums1 = [1,2,3,0,0,0], m = 3
+# nums2 = [2,5,6],       n = 3
+# 
+# 输出: [1,2,2,3,5,6] 
+#  Related Topics 数组 双指针 
+#  👍 562 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        i, j= 0,0
+        while j<n:
+            if i>=m+j:
+                nums1[i:i+n-j] = nums2[j:n]
+                break
+            elif nums1[i]<nums2[j]:
+                i +=1
+            else:
+                nums1[i],nums1[i+1:] = nums2[j], nums1[i:len(nums1) -1]
+                i +=1
+                j +=1
+# leetcode submit region end(Prohibit modification and deletion)
