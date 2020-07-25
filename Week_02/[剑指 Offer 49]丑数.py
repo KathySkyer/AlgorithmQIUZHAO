48e264236cf8b3b67d501c85e898b49a5ec48016
KathySkyer <42636545+KathySkyer@users.noreply.github.com>
2020-07-25 22:36:37 +0800

    Add files via upload

---
+++ Week_02/[剑指 Offer 49]丑数.py
@@ -0,0 +1,43 @@
+# 我们把只包含质因子 2、3 和 5 的数称作丑数（Ugly Number）。求按从小到大的顺序的第 n 个丑数。 
+# 
+#  
+# 
+#  示例: 
+# 
+#  输入: n = 10
+# 输出: 12
+# 解释: 1, 2, 3, 4, 5, 6, 8, 9, 10, 12 是前 10 个丑数。 
+# 
+#  说明: 
+# 
+#  
+#  1 是丑数。 
+#  n 不超过1690。 
+#  
+# 
+#  注意：本题与主站 264 题相同：https://leetcode-cn.com/problems/ugly-number-ii/ 
+#  Related Topics 数学 
+#  👍 50 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution(object):
+    def nthUglyNumber(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        dp, a, b, c = [1] * n, 0, 0, 0
+        for i in range(1,n):
+            n2, n3, n5 = 2 * dp[a], 3 * dp[b], 5 * dp[c]
+            dp[i] = min(n2, n3, n5)
+            if dp[i] == n2: a += 1
+            if dp[i] == n3: b += 1
+            if dp[i] == n5: c += 1
+        return dp[-1]
+
+
+# leetcode submit region end(Prohibit modification and deletion)
+q = Solution()
+res = q.nthUglyNumber(14)
+print(res)
